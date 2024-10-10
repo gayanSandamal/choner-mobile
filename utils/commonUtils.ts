669376dfc131fea0aa5cf565299dbfcb2936e3bd
Gayan Sandamal <gayansandamal94@gmail.com>
2024-10-10 23:35:49 +0530

    Bug fixes and improvements

--- utils/commonUtils.ts
+++ utils/commonUtils.ts
@@ -45,16 +45,19 @@ export const formatDateToCustomString = (date: Date) => {
   const options: Intl.DateTimeFormatOptions = {
     hour: 'numeric',
     minute: 'numeric',
-    hour12: true,
+    hour12: false,
     month: 'short',
     day: '2-digit',
     year: 'numeric',
   }
 
   const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date)
-  const [day, year, time] = formattedDate.split(', ')
+  const [day, yearWithTime] = formattedDate.split(', ')
+  const [year, time] = yearWithTime.split(' at ')
 
-  return `${time.trim()} on ${day}, ${year}`
+  console.log(`day ${day}, year ${year}, time ${time}`);
+
+  return `${time?.trim()} on ${day}, ${year}`
 }
 
 export const postCreateTimeToDate = (createdAt: {_seconds: number, _nanoseconds: number}) => {
