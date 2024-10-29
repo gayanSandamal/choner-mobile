86b6f1f99374342605292dfc16f409651038a800
nisalV <nisal.pathmila.perera@gmail.com>
2024-10-29 23:14:40 +0530

    Challenge home

--- app/(app)/(tabs)/challenges/index.tsx
+++ app/(app)/(tabs)/challenges/index.tsx
@@ -4,20 +4,25 @@ import Label from '@/components/Base/Label'
 import { UserStatus } from '@/components/Common/UserStatus'
 import { Colors } from '@/constants/Colors'
 import { FontTypes, IconNames, JustifyContent } from '@/types/Components'
-import { ScrollView, ImageBackground,StyleSheet, View } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+import { ScrollView, ImageBackground,StyleSheet, View, Image } from 'react-native'
+import MaskedView from "@react-native-masked-view/masked-view"
 
 const styles = StyleSheet.create({
   eventWrapper: {width: '100%',  minHeight: 180, borderRadius: 10, overflow: 'hidden', borderWidth: 1, borderColor: Colors.dark['grey-shade-3'], backgroundColor: Colors.dark['grey-shade-2']},
   countMeIn: {width: 145, height: 38, paddingHorizontal: 12, borderRadius: 20, borderColor: Colors.light.white},
-  cardWrapper: {width: '100%', aspectRatio: 1.7, position: 'relative', borderRadius: 10, overflow: 'hidden', borderWidth: 1, borderColor: Colors.dark['grey-shade-3'], backgroundColor: Colors.dark['grey-shade-2']},
+  cardWrapper: {width: '100%', aspectRatio: 1.8, position: 'relative', borderRadius: 10, overflow: 'hidden', borderWidth: 1, borderColor: Colors.dark['grey-shade-3'], backgroundColor: Colors.dark['grey-shade-2']},
+  cardWrapper2: {width: '100%', aspectRatio: 2, position: 'relative', borderRadius: 10, overflow: 'hidden', borderWidth: 1, borderColor: Colors.dark['grey-shade-3'], backgroundColor: Colors.dark['grey-shade-2']},
   cardButton1: {width: 100, height: 38, paddingHorizontal: 12, borderRadius: 20, borderColor: Colors.light.white},
-  cardButton2: {width: 100, height: 38, paddingHorizontal: 12, borderRadius: 20, borderColor: Colors.light.white},
-  cardButton3: {width: 100, height: 38, paddingHorizontal: 12, borderRadius: 20, borderColor: Colors.light.white},
+  cardButton2: {width: 120, height: 38, paddingHorizontal: 12, borderRadius: 20, borderColor: Colors.light.white},
+  cardButton3: {width: 150, height: 38, paddingHorizontal: 12, borderRadius: 20, borderColor: Colors.light.white},
 })
 
 
 const imageUrl1 = 'https://images.pexels.com/photos/1141853/pexels-photo-1141853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
 const imageUrl2 = 'https://img.freepik.com/premium-photo/silhouette-sports-girl-running-along-road-sunset-her-legs-motion-against-vibrant-sky_1162141-31348.jpg'
+const imageUrl3 = 'https://img.freepik.com/free-photo/portrait-army-during-medieval-times_23-2150932123.jpg'
+const imageUrl4 = 'https://t4.ftcdn.net/jpg/00/23/45/23/360_F_23452393_7GQXDsWyrnCEEWQq4JYgjPEKbvW1FbRC.jpg'
 
 export default function ChallengesScreen() {
   return (
@@ -38,37 +43,35 @@ export default function ChallengesScreen() {
       <View className='w-full mt-[10px] mb-[25px]'>
         <ImageBackground src={imageUrl2} resizeMode="cover" style={styles.cardWrapper}>
           <View className='w-full h-full px-4 pt-5 pb-2' style={{backgroundColor: '#0000006A'}}>
-            <Label containerStyles={{fontSize: 24, letterSpacing: 3, fontWeight: 700}} color={Colors.dark.main} label='ON-GOING' />
-            <Label classNames='pb-0.5' containerStyles={{fontSize: 24, letterSpacing: 3, fontWeight: 700}} color={Colors.dark.main} label='CHALLENGES' />
-            <View style={{position: 'absolute', bottom: 25, width: '100%', alignItems: 'center'}}>
-              <BtnDetailed label='Explore' fontType={FontTypes.FTitle3} labelAlign={JustifyContent.center} wrapperStyle={styles.cardButton1} onPress={() => {}} />
-            </View>
+            <Label containerStyles={{fontSize: 20, letterSpacing: 3, fontWeight: 700}} color={Colors.dark.main} label='ON-GOING' />
+            <Label classNames='pb-0.5' containerStyles={{fontSize: 20, letterSpacing: 3, fontWeight: 700}} color={Colors.dark.main} label='CHALLENGES' />
+          </View>
+          <View style={{position: 'absolute', bottom: 10, width: '100%', alignItems: 'center'}}>
+            <BtnDetailed label='Explore' fontType={FontTypes.FTitle3} labelAlign={JustifyContent.center} wrapperStyle={styles.cardButton1} onPress={() => {}} />
           </View>
         </ImageBackground>
       </View>
 
       
       <View className='w-full mb-[25px]'>
-        <ImageBackground src={imageUrl2} resizeMode="cover" style={styles.cardWrapper}>
-          <View className='w-full h-full px-4 pt-5 pb-2' style={{backgroundColor: '#0000006A'}}>
-            <Label containerStyles={{fontSize: 24, letterSpacing: 3, fontWeight: 700}} color={Colors.dark.main} label='ON-GOING' />
-            <Label classNames='pb-0.5' containerStyles={{fontSize: 24, letterSpacing: 3, fontWeight: 700}} color={Colors.dark.main} label='CHALLENGES' />
-            <View style={{position: 'absolute', bottom: 25, width: '100%', alignItems: 'center'}}>
-              <BtnDetailed label='Find Yours' fontType={FontTypes.FTitle3} labelAlign={JustifyContent.center} wrapperStyle={styles.cardButton2} onPress={() => {}} />
-            </View>
+        <ImageBackground src={imageUrl4} resizeMode="cover" style={styles.cardWrapper2}>
+          <View className='w-full h-full px-4 pt-5 pb-2 items-center justify-center' style={{backgroundColor: '#0000006A'}}>
+            <Label classNames='pb-10' containerStyles={{fontSize: 32, letterSpacing: 3, fontWeight: 700}} color={Colors.dark['soundcloud-gdr-1']} label='CHOSEN ONE' />
+          </View>
+          <View style={{position: 'absolute', bottom: 10, width: '100%', alignItems: 'center'}}>
+            <BtnDetailed label='Find Yours' fontType={FontTypes.FTitle3} labelAlign={JustifyContent.center} wrapperStyle={styles.cardButton2} onPress={() => {}} />
           </View>
         </ImageBackground>
       </View>
 
       
       <View className='w-full mb-[20px]'>
-        <ImageBackground src={imageUrl2} resizeMode="cover" style={styles.cardWrapper}>
-          <View className='w-full h-full px-4 pt-5 pb-2' style={{backgroundColor: '#0000006A'}}>
-            <Label containerStyles={{fontSize: 24, letterSpacing: 3, fontWeight: 700}} color={Colors.dark.main} label='ON-GOING' />
-            <Label classNames='pb-0.5' containerStyles={{fontSize: 24, letterSpacing: 3, fontWeight: 700}} color={Colors.dark.main} label='CHALLENGES' />
-            <View style={{position: 'absolute', bottom: 25, width: '100%', alignItems: 'center'}}>
-              <BtnDetailed label='Can You Resist?' fontType={FontTypes.FTitle3} labelAlign={JustifyContent.center} wrapperStyle={styles.cardButton3} onPress={() => {}} />
-            </View>
+        <ImageBackground src={imageUrl3} resizeMode="cover" style={styles.cardWrapper2}>
+          <View className='w-full h-full px-4 pt-5 pb-2 items-center justify-center' style={{backgroundColor: '#0000006A'}}>
+            <Label classNames='pb-10' containerStyles={{fontSize: 24, letterSpacing: 3, fontWeight: 700}} color={Colors.light.white} label='THE RESISTANCE' />
+          </View>
+          <View style={{position: 'absolute', bottom: 10, width: '100%', alignItems: 'center'}}>
+            <BtnDetailed label='Can You Resist?' fontType={FontTypes.FTitle3} labelAlign={JustifyContent.center} wrapperStyle={styles.cardButton3} onPress={() => {}} />
           </View>
         </ImageBackground>
       </View>
