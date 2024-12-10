4e02f4350ff2caa750a491bfbfa1b5f652551ea5
Gayan Sandamal <gayansandamal94@gmail.com>
2024-12-10 23:17:09 +0530

    basic info focusing

--- components/ProfileOptions/UpdateBasicInfo.tsx
+++ components/ProfileOptions/UpdateBasicInfo.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { Input } from "../Base/Input"
-import { View, StyleSheet, FlatList, Text, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from "react-native"
+import { View, StyleSheet, FlatList, Text, TouchableOpacity, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform } from "react-native"
 import { Btn, BtnDetailed } from "../Base/Button"
 import { FontSizes, IconNames, InputSizes, UploadImage } from "@/types/Components"
 import { Colors } from "@/constants/Colors"
@@ -19,10 +19,10 @@ import { StoragePaths } from "@/constants/values"
 import { Toast } from "toastify-react-native"
 
 const styles = StyleSheet.create({
-    avatarWrapper: {marginLeft: 'auto', marginRight: 'auto'},
+    avatarWrapper: { marginLeft: 'auto', marginRight: 'auto' },
     avatar: { marginTop: 20, backgroundColor: '#F7971E' },
-    cameraButton: {position: 'absolute', bottom: 0, right: -3, padding: 6, borderRadius: 8, backgroundColor: Colors.dark['grey-transparent']},
-    dropDownWrapper: {zIndex: 1},
+    cameraButton: { position: 'absolute', bottom: 0, right: -3, padding: 6, borderRadius: 8, backgroundColor: Colors.dark['grey-transparent'] },
+    dropDownWrapper: { zIndex: 1 },
     btnWrapper: { width: '100%', paddingHorizontal: 25, borderRadius: 30, height: 60, marginBottom: 20, backgroundColor: Colors.dark['field-bg-idle'], flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', borderWidth: 0 },
     dropdown: { position: 'absolute', marginTop: 70, backgroundColor: Colors.dark['field-bg-idle'], borderRadius: 5, width: '100%', zIndex: 2 },
     dropdownItem: { padding: 15 },
@@ -31,19 +31,19 @@ const styles = StyleSheet.create({
 
 const professions = ['LIFE COACH', 'MENTOR', 'FITNESS COACH', 'YOGA EXPERT']
 
-const imagePromptText = [{icon: IconNames.camera, text: 'Take a photo'}, {icon: IconNames.gallery, text: 'Choose from gallery'}]
+const imagePromptText = [{ icon: IconNames.camera, text: 'Take a photo' }, { icon: IconNames.gallery, text: 'Choose from gallery' }]
 
 export default function SettingsHome() {
-    const {user} = useUser()
+    const { user } = useUser()
 
     const [firstName, setFirstName] = useState<string>(user?.displayName?.split(' ')?.[0] || '')
     const [lastName, setLastName] = useState<string>(user?.displayName?.split(' ')?.[1] || '')
-    const [image, setImage] = useState< UploadImage| null>(null)
+    const [image, setImage] = useState<UploadImage | null>(null)
     const [isUpdating, setIsupdating] = useState<boolean>(false)
     const [showDrawer, setShowDrawer] = useState<boolean>(false)
 
-    const {mutate: updateUser} = useSetUser(() => onSuccess(), () => onError())
-    const {uploadImage} = useUploadImage((uri) => updateUserWithImage(uri), (e) => onUploadImageError(e))
+    const { mutate: updateUser } = useSetUser(() => onSuccess(), () => onError())
+    const { uploadImage } = useUploadImage((uri) => updateUserWithImage(uri), (e) => onUploadImageError(e))
 
     const checkAndSetFirstName = (text: string) => {
         if (isUpdating) return
@@ -56,22 +56,22 @@ export default function SettingsHome() {
     }
 
     const onSave = async () => {
-        setIsupdating(true)  
+        setIsupdating(true)
 
         if (user && image?.blob) {
             await uploadImage(image, StoragePaths.USER)
         }
-        
+
         user && !image?.blob && updateUser(commonUploadData())
     }
 
     const updateUserWithImage = (uri: string) => {
         const newUrl = uri.replace(`.${image?.type}`, '_200x200.png')
-        updateUser({...commonUploadData(), profileImageUrl: newUrl})
+        updateUser({ ...commonUploadData(), profileImageUrl: newUrl })
     }
 
     const onUploadImageError = (e: any) => {
-        setIsupdating(false)  
+        setIsupdating(false)
         setImage(null)
     }
 
@@ -82,7 +82,7 @@ export default function SettingsHome() {
 
     const onError = () => {
         setImage(null)
-        setIsupdating(false)  
+        setIsupdating(false)
         setFirstName(user?.displayName?.split(' ')?.[0] || '')
         setLastName(user?.displayName?.split(' ')?.[1] || '')
     }
@@ -104,10 +104,10 @@ export default function SettingsHome() {
             aspect: [1, 1],
             quality: 1,
         })
-    
+
         if (!result.canceled) await setImageData(result)
     }
-    
+
     const captureAndPickImage = async () => {
         await ImagePicker.requestCameraPermissionsAsync()
         await MediaLibrary.requestPermissionsAsync()
@@ -125,7 +125,7 @@ export default function SettingsHome() {
         const imageUri = image?.assets?.[0].uri || ''
         const imageType = image?.assets?.[0]?.fileName?.split('.').slice(-1)[0] || 'jpg'
         const imageBlob = await getBlobFromUri(imageUri)
-    
+
         setImage({
             uri: imageUri,
             name: `${user?.uid}.${imageType}`,
@@ -135,31 +135,36 @@ export default function SettingsHome() {
     }
 
     const onPressImagePickItem = (index: number) => {
-        index === 0 ? captureAndPickImage(): pickImage()
+        index === 0 ? captureAndPickImage() : pickImage()
         setShowDrawer(false)
     }
-    
+
     return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-        <View style={{flex: 1}}>
-            <View style={styles.avatarWrapper}>
-                <Avatar img={image?.uri || user?.profileImageUrl} containerStyles={styles.avatar} size={InputSizes.xl} onPressAvatar={() => setShowDrawer(true)}/>
-                <TouchableOpacity disabled={isUpdating} style={styles.cameraButton} onPress={() => setShowDrawer(true)}>
-                    <Icon name={IconNames.camera} />
-                </TouchableOpacity>
-            </View>
-            <Label classNames='text-white mt-6 mb-3' label="Registered email" />
-            <Input classNames='mb-5' placeholder={'First name'} value={user?.email} disabled onChange={checkAndSetFirstName} />
-            <Label classNames='text-white mb-3' label="First name" />
-            <Input classNames='mb-5' placeholder={'First name'} value={firstName} onChange={checkAndSetFirstName} />
-            <Label classNames='text-white mb-3' label="Last name" />
-            <Input classNames='mb-8' placeholder={'Last name'} value={lastName} onChange={checkAndSetLastName} />
-            <View className="ml-0.5 mr-0.5 flex-row justify-between">
-                <Btn outlined disabled={isUpdating} onPress={() => router.back()} icon={IconNames.cancel} size={InputSizes.md} color={Colors.dark['green-shade-1']} label="CANCEL" />
-                <Btn isLoading={isUpdating} disabled={isUpdating} onPress={onSave} icon={IconNames.save} size={InputSizes.md} backgroundColor={Colors.dark['green-shade-1']} label="SAVE" />
-            </View>
-            <ImagePickerBottomDrawer showDrawer={showDrawer} setShowDrawer={setShowDrawer} onPressImagePickItem={onPressImagePickItem} />
-        </View>
-    </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+            <>
+                <View style={styles.avatarWrapper}>
+                    <Avatar img={image?.uri || user?.profileImageUrl} containerStyles={styles.avatar} size={InputSizes.xl} onPressAvatar={() => setShowDrawer(true)} />
+                    <TouchableOpacity disabled={isUpdating} style={styles.cameraButton} onPress={() => setShowDrawer(true)}>
+                        <Icon name={IconNames.camera} />
+                    </TouchableOpacity>
+                </View>
+                <Label classNames='text-white mt-6 mb-3' label="Registered email" />
+                <Input classNames='mb-5' placeholder={'First name'} value={user?.email} disabled onChange={checkAndSetFirstName} />
+                <KeyboardAvoidingView
+                    style={{ flex: 1 }}
+                    behavior={Platform.OS === "ios" ? "padding" : "height"}
+                >
+                    <Label classNames='text-white mb-3' label="First name" />
+                    <Input classNames='mb-5' placeholder={'First name'} value={firstName} onChange={checkAndSetFirstName} />
+                    <Label classNames='text-white mb-3' label="Last name" />
+                    <Input classNames='mb-8' placeholder={'Last name'} value={lastName} onChange={checkAndSetLastName} />
+                    <View className="ml-0.5 mr-0.5 flex-row justify-between">
+                        <Btn outlined disabled={isUpdating} onPress={() => router.back()} icon={IconNames.cancel} size={InputSizes.md} color={Colors.dark['green-shade-1']} label="CANCEL" />
+                        <Btn isLoading={isUpdating} disabled={isUpdating} onPress={onSave} icon={IconNames.save} size={InputSizes.md} backgroundColor={Colors.dark['green-shade-1']} label="SAVE" />
+                    </View>
+                    <ImagePickerBottomDrawer showDrawer={showDrawer} setShowDrawer={setShowDrawer} onPressImagePickItem={onPressImagePickItem} />
+                </KeyboardAvoidingView>
+            </>
+        </TouchableWithoutFeedback>
     )
 }
